414278a9df75fcc4fe0673f77adafd8687ffee30
nirojEdulab <niroj@edulab.in>
2024-11-10 15:55:35 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -2,22 +2,15 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
+const morgan = require('morgan');
 const productRoutes = require("./routes/productRoutes");
 
 dotenv.config();
 const app = express();
-morgan.format("custom", ":date[iso] :method :url");
-app.use(morgan("custom"));
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-app.use(
-  "/public/uploads",
-  express.static(path.join(__dirname, "/public/uploads"))
-);
+morgan.format('custom', ':date[iso] :method :url');
+app.use(morgan('custom'));
+app.use(cors({ origin: process.env.CLIENT_API, credentials: true }));
+app.use("/public/uploads", express.static(path.join(__dirname, "/public/uploads")));
 app.use(express.json());
 
 // Product routes
